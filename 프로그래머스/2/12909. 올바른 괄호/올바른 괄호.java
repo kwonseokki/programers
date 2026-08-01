3e423e8758c7b82c1e4aa6bc78585c7d9e1833e7
kwonseokki <kwons41635841@gmail.com>
2026-08-01 12:47:09 +0900

    [level 2] Title: 올바른 괄호, Time: 11.93 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,14 +2,13 @@ import java.util.Stack;
 
 class Solution {
     boolean solution(String s) {
-                Stack<Character> stack = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (!stack.isEmpty() && c == ')' && stack.peek() == '(') {
-                stack.pop();
-            } else {
-                stack.push(c);
-            }
+        Stack<Character> stack = new Stack<>();
+        // "(())()"
+        // 괄호가 열려있을때 닫힌 괄호가 들어온다
+        // 스택이 비어있다면 올바른 괄호
+        for (char  c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == '(' && c == ')') stack.pop();
+            else stack.push(c);
         }
 
         return stack.isEmpty();
